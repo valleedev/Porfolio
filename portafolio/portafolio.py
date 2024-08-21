ee8bc21d1445a701071853a20454ea51b4527fd7
ValleDeve <vallebarbaranj@gmail.com>
2024-08-21 12:09:57 -0500

    Se añade script de google analitycs

--- portafolio/portafolio.py
+++ portafolio/portafolio.py
@@ -1,40 +1,39 @@
-import reflex as rx 
+import reflex as rx
 import json
-import portafolio.styles.styles as styles 
-from portafolio.views.navbar import navbar  
-from portafolio.components.header import header  
-from portafolio.components.about_me import about_me  
-from portafolio.components.experience import experience  
-from portafolio.components.skills import skills  
-from portafolio.components.projects import projects  
-from portafolio.views.footer import footer  
+import portafolio.styles.styles as styles
+from portafolio.views.navbar import navbar
+from portafolio.components.header import header
+from portafolio.components.about_me import about_me
+from portafolio.components.experience import experience
+from portafolio.components.skills import skills
+from portafolio.components.projects import projects
+from portafolio.views.footer import footer
 
-def index() -> rx.Component: 
-    return rx.box(  
+def index() -> rx.Component:
+    return rx.box(
         navbar(),
-        rx.center(  
-            header(), 
-            about_me(), 
-            experience(),  
-            projects(), 
-            skills(),  
-            flex_direction="column", 
-            style={"overflow": "auto"}, 
-            bg=styles.Color.BGCOLOR.value,  
+        rx.center(
+            header(),
+            about_me(),
+            experience(),
+            projects(),
+            skills(),
+            flex_direction="column",
+            style={"overflow": "auto"},
+            bg=styles.Color.BGCOLOR.value,
         ),
-        footer(),  
-        scroll_behavior="smooth" 
+        footer(),
+        scroll_behavior="smooth"
     )
 
 app = rx.App(
-    stylesheets=styles.STYLESHEETS,  
-    style=styles.BASE_STYLE  
+    stylesheets=styles.STYLESHEETS,
+    style=styles.BASE_STYLE
 )
 
 title = "Sebastian Valle / Portfolio"
 description = "Hola mi nombre es Sebastian Valle desarrollador de software, tambien hago parte de la iglesia Adventista del Séptimo Dia."
-canonical_url = "https://sebasvalle.vercel.app/" 
-
+canonical_url = "https://sebasvalle.vercel.app/"
 
 schema_markup = {
     "@context": "https://schema.org",
@@ -67,7 +66,17 @@ app.add_page(
         {"name": "og:description", "content": description},
         {"name": "twitter:card", "content": "summary_large_image"},
         {"name": "twitter:site", "content": "@valledev"},
-        {"rel": "canonical", "href": canonical_url}, 
-        {"type": "application/ld+json", "content": json.dumps(schema_markup)} 
+        {"rel": "canonical", "href": canonical_url},
+        {"type": "application/ld+json", "content": json.dumps(schema_markup)},
+        # Agregar el código de Google Analytics
+        {"src": "https://www.googletagmanager.com/gtag/js?id=G-T900HM6T4F", "async": "true"},
+        {
+            "content": """
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-T900HM6T4F');
+            """
+        },
     ]
 )
